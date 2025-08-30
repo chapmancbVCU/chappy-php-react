f22a1ef2a9273a7306dd0a1b4bea30d029c9c7a9
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-29 20:33:37 -0400

    Moved function to get csrf token to core

--- resources/views/react/host.php
+++ resources/views/react/host.php
@@ -1,20 +1,11 @@
 <?php $this->start('head') ?>
-  <?= Core\Lib\React\Vite::tags('resources/js/app.jsx'); ?>
-
-  <?php
-  function csrf_value() {
-    $html = csrf();
-    if (preg_match('/value="([^"]+)"/', (string)$html, $m)) return $m[1];
-    return '';
-}
-$token = csrf_value(); 
-  ?>
+<?= Core\Lib\React\Vite::tags('resources/js/app.jsx'); ?>
 <?php $this->end(); ?>
 
 <?php $this->start('body') ?>
   <div
     id="app"
-    data-csrf="<?= htmlspecialchars($token, ENT_QUOTES, 'UTF-8') ?>"
+    data-csrf="<?= Core\Lib\React\Vite::csrfToken() ?>"
     data-component="<?= htmlspecialchars($this->component ?? 'home', ENT_QUOTES, 'UTF-8'); ?>"
     data-props='<?= htmlspecialchars(json_encode($this->props ?? []), ENT_QUOTES, "UTF-8"); ?>'>
   </div>
