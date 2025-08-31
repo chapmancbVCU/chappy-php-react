72229aab8a05cf32e287a99db69dffbb2212025c
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-31 13:46:27 -0400

    Added helper functions for trimming forward slashes from domain and path parts

--- resources/js/utils/route.js
+++ resources/js/utils/route.js
@@ -10,17 +10,30 @@ export default function route(path, params = []) {
     const controller = parts[0];
     const action = (parts.length === 2) ? parts[1] : 'index';
 
-    let domain = import.meta.env.VITE_APP_DOMAIN;
-
-    if(domain.endsWith('/')) {
-        domain = domain.slice(0, -1);
-    }
-
-    let url = domain + '/' + controller.replace(/\//g, "") + "/" + action.replace(/\//g, "");
+    let domain = trimDomain(import.meta.env.VITE_APP_DOMAIN);
+    let url = domain + '/' + trimParts(controller) + "/" + trimParts(action);
 
     if(url.length !== 0) {
         url += '/' + params.map(encodeURIComponent).join('/');
     } 
 
     return url;
+}
+
+/**
+ * Trims domain if it contains trailing forward slashes.
+ * @param {string} domain The application domain.
+ * @returns {string} The updated domain string.
+ */
+function trimDomain(domain) {
+    return (domain.endsWith('/')) ? domain.slice(0, -1) : domain;
+}
+
+/**
+ * Trims any forward slashes from action or controller names.
+ * @param {string} part The part of the URL to be trimmed.
+ * @returns {string} The trimmed action or controller name.
+ */
+function trimParts(part) {
+    return part.replace(/\//g, "");
 }
