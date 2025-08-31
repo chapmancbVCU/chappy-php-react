ad55369c74a949e3b603c0ada0bdd4bbc7d403cf
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-31 14:50:45 -0400

    #2 - Initial setup of function and begin form

--- resources/js/pages/profile/Edit.jsx
+++ resources/js/pages/profile/Edit.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import '@css/profileImage.css';
+import { CSRF } from "@/utils/FormHelpers";
 
-function Edit() {
+function Edit({user, displayMessages, profileImages}) {
     return (
-        <>
-        </>
+        <div className="row align-items-center justify-content-center mb-5">
+            <div className="col-md-6 bg-light p-3">
+                <h1 className="text-center">Edit Details for {user.username}</h1>
+                <hr />
+                <form className="form" action="" method="post" encType="multipart/form-data">
+                    <CSRF />
+                </form>
+            </div>
+        </div>
     );
 }
 
