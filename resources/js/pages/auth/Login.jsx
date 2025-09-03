77fca38f476a4a15302a59c97994cbcec6e7bc24
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-02 20:25:46 -0400

    #3 - Initial commit

---
+++ resources/js/pages/auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import route from "@/utils/route";
+function Auth({errors, login}) {
+
+    return(
+        <div className="row align-items-center justify-content-center">
+            <div className="col-md-6 bg-light p-3">
+                <h1 className="text-center">Login</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Auth;
