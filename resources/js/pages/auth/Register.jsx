8d9b16b2884f58055d3e95ddaade4f7f1cd8e0bf
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-05 20:35:08 -0400

    #4 - Implemented register component

--- resources/js/pages/auth/Register.jsx
+++ resources/js/pages/auth/Register.jsx
@@ -1,10 +1,94 @@
 import React from "react";
-
 import Forms from "@/components/Forms";
-import route from "@/utils/route";
 
-function Register({user}) {
-
+/**
+ * Generates the register view component
+ * @property {object} user The user model object.
+ * @property {object} 
+ * @param {InputProps} param0 
+ * @returns {HTMLDivElement} The register view component.
+ */
+function Register({user, errors}) {
+    return (
+        <div className="row align-items-center justify-content-center">
+            <div className="col-md-6 bg-light p-3">
+                <h3 class="text-center">Register Here!</h3>
+                <hr />
+                <form action="" className="form" method="post" enctype="multipart/form-data">
+                    <Forms.CSRF />
+                    <Forms.DisplayErrors errors={errors}/>
+                    <Forms.Input 
+                        type={"text"}
+                        label={"User name"}
+                        name={"username"}
+                        value={user.username}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.Input 
+                        type={"text"} 
+                        label={"First Name"} 
+                        name={"fname"}
+                        value={user.fname}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.Input 
+                        type={"text"} 
+                        label={"Last Name"} 
+                        name={"lname"}
+                        value={user.lname}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.Input 
+                        type={"email"} 
+                        label={"Email"} 
+                        name={"email"}
+                        value={user.email}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.RichText
+                        label="Description"
+                        name="description"
+                        value={user.description || ""}
+                        inputAttrs={{ placeholder: 'Describe yourself here...' }}
+                        divAttrs={{ className: 'form-group mb-3' }}
+                    />
+                    <Forms.Input 
+                        type={"file"}
+                        label={"Upload Profile Image (Optional"}
+                        name={"profileImage"}
+                        value={""}
+                        inputAttrs={{className: 'form-control', accept: 'image/gif image/jpeg image/png'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.Input 
+                        type={"password"}
+                        label={"Password"}
+                        name={"password"}
+                        value={user.password}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.Input 
+                        type={"password"}
+                        label={"Confirm Password"}
+                        name={"confirm"}
+                        value={user.confirm}
+                        inputAttrs={{className: 'form-control input-sm'}}
+                        divAttrs={{className: 'form-group mb-3'}}
+                    />
+                    <Forms.SubmitBlock
+                        label={"Register"}
+                        inputAttrs={{className: 'btn btn-large btn-primary'}}
+                        divAttrs={{className: 'text-end'}}
+                    />
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Register;
