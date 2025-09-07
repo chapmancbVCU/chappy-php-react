d0b25525dca12329f365d2be2b4b2ee8c8d7a72b
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-07 17:59:32 -0400

    #5 - Imported Forms and PasswordComplexityRequirements components

--- resources/js/pages/profile/UpdatePassword.jsx
+++ resources/js/pages/profile/UpdatePassword.jsx
@@ -1,4 +1,6 @@
-import React from "react"
+import React from "react";
+import Forms from "@chappy/components/Forms";
+import {PasswordComplexityRequirements} from '@chappy/components/PasswordComplexityRequirements'
 function UpdatePassword({user, errors}) {
 
     return (
